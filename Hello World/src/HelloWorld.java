c94d8fd1ae5cd830f61acbdf0654b958ef9a68a4
Vaibhav Sakhalkar <49814913+sakhalkar@users.noreply.github.com>
2019-04-20 22:41:51 +0530

    Initial version

---
+++ Hello World/src/HelloWorld.java
@@ -0,0 +1,23 @@
+
+public class HelloWorld 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		int i =0;
+		// TODO Auto-generated method stub
+		System.out.println("Hello Vaibhav, Welcome back to Java world!");
+
+		for(i=0;i<10;i++)
+		{
+			
+			System.out.println("i = " + i);			
+		}
+		
+		System.out.println("Hope you have learned something new today!");
+
+	
+	}
+
+}
